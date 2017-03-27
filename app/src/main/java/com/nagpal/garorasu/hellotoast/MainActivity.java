f8f936ab9f8662b54a9f97bef088d344172408af
rohitrfactor <rohitrfactor@gmail.com>
2017-03-27 11:47:43 +0530

    Count Up Button update view

--- app/src/main/java/com/nagpal/garorasu/hellotoast/MainActivity.java
+++ app/src/main/java/com/nagpal/garorasu/hellotoast/MainActivity.java
@@ -4,10 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private int mCount;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d(LOG_TAG,"Activity Started");
         setContentView(R.layout.activity_main);
         Log.d(LOG_TAG,"Layout inflated");
+        mShowCount	=	(TextView)	findViewById(R.id.show_count);
     }
 
     public void showToast(View view) {
@@ -24,5 +30,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void countUp(View view) {
         Log.d(LOG_TAG,"Count Button pressed");
+        mCount++;
+        if(mShowCount != null)
+        {
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
