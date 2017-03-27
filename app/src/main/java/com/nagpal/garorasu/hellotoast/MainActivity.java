d0be61455298561d7c30e73f16c086df8cf3bd3e
rohitrfactor <rohitrfactor@gmail.com>
2017-03-27 11:43:31 +0530

    Toast added to Toast Button

--- app/src/main/java/com/nagpal/garorasu/hellotoast/MainActivity.java
+++ app/src/main/java/com/nagpal/garorasu/hellotoast/MainActivity.java
@@ -2,12 +2,27 @@ package com.nagpal.garorasu.hellotoast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG,"Activity Started");
         setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG,"Layout inflated");
+    }
+
+    public void showToast(View view) {
+        Log.d(LOG_TAG,"Toast Button pressed");
+        Toast.makeText(getApplicationContext(), R.string.toast_message,Toast.LENGTH_LONG).show();
+    }
+
+    public void countUp(View view) {
+        Log.d(LOG_TAG,"Count Button pressed");
     }
 }
